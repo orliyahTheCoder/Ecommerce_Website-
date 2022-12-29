b40d66cd14506aa53e50762ad30365cd93ef9955
orliyahTheCoder <orliyahisrael@gmail.com>
2022-12-29 08:34:02 +0200

    fisrt commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Layout } from "../components";
+import { Toaster } from "react-hot-toast";
+import { StateContext } from "../context/StateContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'dfcvale9',
+    dataset: 'production'
+  }
+})
